0becf18fa1a2a6b39fb45715e1f85a54097aee4f
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-10-06 13:09:06 +0200

    struttura lastest book aggiunta

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,28 @@
 <template>
   <div id="app">
-    
+    <Header />
   </div>
 </template>
 
 <script>
-
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-
+    Header,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
-  // color: #2c3e50;
-  // margin-top: 60px;
+body{
+  margin: 0;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    
+  }
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,28 @@
 <template>
     <header>
-
+        <div class="container">
+            <div class="row">
+                <!-- header logo -->
+                <div class="col-6">
+                    <img src="images/author-logo-round.png" alt="">
+                </div>
+                <!-- header nav  -->
+                <div class="col-6 d-flex">
+                    <nav>
+                        <ul class="d-flex">
+                            <!-- aggiungere v-for headerLinkArray  -->
+                            <li v-for="(element, index) in headerLinkArray" :key="index"><a href="element.link">{{element.name}}</a></li>
+                            <li><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div id="lastest_book_release"></div>   
+            </div>
+        </div>
     </header>
 </template>
 
@@ -12,9 +34,34 @@ export default {
             //array contenente le informazioni presenti nell'header 
             headerLinkArray: [
                 {
-                    name: '',
-                    link: ''
+                    name: 'Home',
+                    link: '#'
                 },
+                {
+                    name: 'About Me',
+                    link: '#'
+                },
+                {
+                    name: 'Testimonials',
+                    link: '#'
+                },
+                {
+                    name: 'My Blog',
+                    link: '#'
+                },
+                {
+                    name: 'Meetups',
+                    link: '#'
+                },
+                {
+                    name: 'Shop',
+                    link: '#'
+                },
+                {
+                    name: 'Contact Me',
+                    link: '#'
+                },
+                
             ],
         }
     },
@@ -23,5 +70,31 @@ export default {
 
 <style lang="scss" scoped>
 @import '~bootstrap/scss/bootstrap.scss';
+@import '~@fortawesome/fontawesome-free/css/all.css';
+header{
+    background: url('../assets/hero-07-2x.jpg'), linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.9));
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 85vh;
+    position: relative;
+
+    img{
+        width: 200px;
+    }
+    #lastest_book_release{
+        width: 650px;
+        height: 50%;
+        background-color: white;
+        position: absolute;
+        top: 101%;
+        left: 100%;
+        transform: translate(-100%, -100%);
+    }
+}
+
+
+
+
 
 </style>
