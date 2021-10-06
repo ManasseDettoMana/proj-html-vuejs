e9f48dc6caa84c62476ee12337d6c0539efe0772
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-10-06 17:34:33 +0200

    refactoring header's lbr

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,10 @@
 <template>
-    <header class="p-relative">
-        <div class="container">
+    <header>
+        <div class="my_container">
             <div class="row">
                 <!-- header logo -->
                 <div class="col-6">
-                    <img src="images/author-logo-round.png" alt="" href="#">
+                    <img src="images/author-logo-round.png" alt="" href="#" id="header_logo">
                 </div>
                 <!-- header nav  -->
                 <div class="col-6">
@@ -18,7 +18,40 @@
                 </div>
             </div>
         </div>
-        <div id="latest_book_release">
+        <div class="my_container" id="bottom_container">
+            <div class="row" id="principal_row">
+                <div class="col-7">
+                    <div class="my_container">
+                        <div class="row">
+                            <h1>asdgasjdas</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae culpa omnis modi iure ratione tempore, exercitationem sequi. Dolorem laboriosam officia, perferendis corporis culpa ut, doloribus repellendus ipsum sunt dolorum ipsa!</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet vitae sequi consequuntur, eaque dolorem delectus eum pariatur accusantium iusto eveniet, velit vero magni ullam est hic beatae? Quas, neque delectus?</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-5">
+                    <div class="row h-30"></div>
+                    <!-- background bianco qui -->
+                    <div class="row " id="lbr">
+                        <div class="my_container d-flex justify-content-center align-items-center flex-wrap pt-5" id="">
+                            <div class="col-6">
+                                <img src="images/book-widget.png" alt="">
+                            </div>
+                            <div class="col-6">
+                                <h2>latest book release</h2>
+                                <h3>d. vaughn autobiography</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ratione illo veritatis odit voluptate aperiam totam, sint voluptates ut quia illum placeat, distinctio modi commodi error, cupiditate repellat. Autem, delectus!</p>
+                            </div>
+                            <div class="col-12">
+                                <img src="images/hover-button1.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- <div id="latest_book_release">
             <div class="container">
                 <div class="row justify-content-center" id="riga__">
                     <div class="col-6">
@@ -32,7 +65,7 @@
                     <img src="images/hover-button1.png" alt="" id="pay_button">
                 </div>
             </div>
-        </div>
+        </div> -->
     </header>
 </template>
 
@@ -90,6 +123,8 @@ export default {
 @import '~bootstrap/scss/bootstrap.scss';
 @import '~@fortawesome/fontawesome-free/css/all.css';
 @import './style/variables.scss';
+
+
 header{
     background-image:linear-gradient(to bottom, rgba(0,0,0,0.25) 0%,rgba(0,0,0,0.9) 100%), url('../assets/hero-07-2x.jpg') ;
     background-position: center;
@@ -97,10 +132,15 @@ header{
     background-repeat: no-repeat;
     height: 85vh;
     position: relative;
-    .p-relative{
-        position: relative;
+
+    #bottom_container{
+        height: calc(100% - 205.28px);
+        margin-right: 0;
+        #principal_row{
+            height: 100%;
+        }
     }
-    .container{
+    .my_container{
         width: 90%;
         margin: 0 auto;
         nav{
@@ -130,7 +170,7 @@ header{
             }
         }
 
-        img{
+        #header_logo{
             width: 200px;
         }
         .active{
@@ -141,33 +181,13 @@ header{
             }
         }
     }
-    #latest_book_release{
-        width: 600px;
-        height: 50%;
-        background-color: white;
-        position: absolute;
-        top: 101%;
-        left: 100%;
-        transform: translate(-100%, -100%);
-        display: flex;
-        justify-content: center;
-        align-content: center;
-        .container{
-            display: flex;
-            justify-content: center;
-            align-content: center;
-            #riga__{
-                padding-top: 40px;
-                #pay_button{
-                    width: 100%;
-                    height: 100px;
-                }
-                #book_widget{
-                    width: 250px
-                }
-            }
-        }
-    }
+}
+#lbr{
+    background-color: white;
+    height: 65%;
+}
+.h-30{
+    height: 35%;
 }
 
 
